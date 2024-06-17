5e69930740a095d75959ae03051a21dbf028d38e
muevetef <muevetef@email.com>
2024-06-17 13:04:55 +0200

    JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/Users");
 /**
@@ -10,7 +14,7 @@ const User = require("../../models/Users");
 router.get("/", auth, async (req, res) => {
   //recoger el usuario de la base de datos
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id).select("-password");
     res.json(user);
   } catch (error) {
     console.log(error);
@@ -18,6 +22,65 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+/**
+ * @desc Verifica las credenciales del usuario y genera el token de sessión
+ * @route POST api/auth
+ * @acces Public
+ * */
+router.post(
+  "/",
+  [
+    check("email", "Por favor inserte un email válido").isEmail(),
+    check("password", "Se requiere un contraseña").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //mirar si el email está registrado
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Credenciales no válidas email" }] });
+      }
+
+      //comprobamo la contraseña
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Credenciales no válidas" }] });
+      }
+
+      //El usuario está autorizado
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      //Generar un jswebtoken y devolverlo
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.status(201).json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,3 +8,18 @@ const router = express.Router();
 router.get("/", (req, res) => res.send("posts route!!"));
 
 module.exports = router;
+
+/*
+ GET /api/posts -> todos los posts
+
+ GET /api/posts/{id} ->  un post por id
+
+ POST /api/posts -> crea un post
+
+ PUT /api/posts/{id} -> actualizar un post
+
+ DELETE /api/posts/{id} -> elimina un post
+
+
+ - El usuario debe estar logeado para crear, editar o eliminar
+*/
